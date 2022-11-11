a2d109d03e4ff7c2dd4e8675deaa923ef77a4665
Cristóbal Bergillos Navarro <crisber@MacBook-Air-de-Cristobal.local>
2022-11-11 22:28:18 +0100

    conectar al server en local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const URL = 'ws://192.168.0.16:8080';
+
+const App = () => {
+  const [user, setUser] = useState('Tarzan');
+  const [message, setMessage] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [ws, setWs] = useState(new WebSocket(URL));
+
+  const submitMessage = (usr, msg) => {
+    const message = { user: usr, message: msg };
+    ws.send(JSON.stringify(message));
+    setMessages([message, ...messages]);
+  }
+
+  useEffect(() => {
+    ws.onopen = () => {
+      console.log('WebSocket Connected');
+    }
+
+    ws.onmessage = (e) => {
+      const message = JSON.parse(e.data);
+      setMessages([message, ...messages]);
+    }
+
+    return () => {
+      ws.onclose = () => {
+        console.log('WebSocket Disconnected');
+        setWs(new WebSocket(URL));
+      }
+    }
+  }, [ws.onmessage, ws.onopen, ws.onclose, messages]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="user">
+        Name :
+        <input
+          type="text"
+          id="user"
+          placeholder="User"
+          value={user}
+          onChange={e => setUser(e.target.value)}
+        />
+      </label>
+
+      <ul>
+        {messages.reverse().map((message, index) =>
+          <li key={index}>
+            <b>{message.user}</b>: <em>{message.message}</em>
+          </li>
+        )}
+      </ul>
+
+      <form
+        action=""
+        onSubmit={e => {
+          e.preventDefault();
+          submitMessage(user, message);
+          setMessage([]);
+        }}
+      >
+        <input
+          type="text"
+          placeholder={'Type a message ...'}
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+        />
+        <input type="submit" value={'Send'} />
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
+
